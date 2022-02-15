0300d7577eddd6b6bd6906fb494bfc95048b539e
brett <99286592+Macroblank@users.noreply.github.com>
2022-02-15 14:55:52 -0500

    Create Album information.py

---
+++ Album information.py
@@ -0,0 +1,7 @@
+albumName = input("Enter album name ")
+albumArtist = input("Enter artist name ")
+albumDate = input("Enter release date (M,D,YYYY) ")
+
+print("Album title is: ", albumName)
+print("The artist is: ", albumArtist)
+print ("The album was released on: ", albumDate)
