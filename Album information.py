a3b3cc1d5656f43ecc335690d90602f2abeba3b2
brett <99286592+Macroblank@users.noreply.github.com>
2022-03-03 14:04:40 -0500

    Update Album information.py

--- Album information.py
+++ Album information.py
@@ -5,3 +5,4 @@ albumDate = input("Enter release date (M,D,YYYY) ")
 print("Album title is: ", albumName)
 print("The artist is: ", albumArtist)
 print ("The album was released on: ", albumDate)
+#basically the mad lib thing we did
